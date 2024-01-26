fcf38e46c31b01f6834a0dbaa0dbb08aca59e9c2
Stefan K. Seritan <sserita@sandia.gov>
2024-01-26 13:49:58 -0800

    Black formatting and attempt for black in CI/CD

--- loqs/codepacks/d3_surface_code.py
+++ loqs/codepacks/d3_surface_code.py
@@ -17,25 +17,54 @@ from loqs.core.syndromecircuit import StabilizerPlaquetteFactory, SyndromeCircui
 surface_factory = StabilizerPlaquetteFactory(
     {
         # Fig 2a
-        "X": Circuit([('Gh', 'aux'), ('Gcnot', 'aux', 'b'), ('Gcnot', 'aux', 'a'), 
-                      ('Gcnot', 'aux', 'd'), [], ('Gcnot', 'aux', 'c'), ('Gh', 'aux'),
-                      ('Iz', 'aux')],
-                     line_labels=['aux', 'a', 'b', 'c', 'd']),
+        "X": Circuit(
+            [
+                ("Gh", "aux"),
+                ("Gcnot", "aux", "b"),
+                ("Gcnot", "aux", "a"),
+                ("Gcnot", "aux", "d"),
+                [],
+                ("Gcnot", "aux", "c"),
+                ("Gh", "aux"),
+                ("Iz", "aux"),
+            ],
+            line_labels=["aux", "a", "b", "c", "d"],
+        ),
         # Fig 2b (including spacers for H layers)
-        "Z": Circuit([[], ('Gcnot', 'b', 'aux'), ('Gcnot', 'a', 'aux'), 
-                      ('Gcnot', 'd', 'aux'), [], ('Gcnot', 'c', 'aux'), [], ('Iz','aux')],
-                     line_labels=['aux', 'a', 'b', 'c', 'd']),
+        "Z": Circuit(
+            [
+                [],
+                ("Gcnot", "b", "aux"),
+                ("Gcnot", "a", "aux"),
+                ("Gcnot", "d", "aux"),
+                [],
+                ("Gcnot", "c", "aux"),
+                [],
+                ("Iz", "aux"),
+            ],
+            line_labels=["aux", "a", "b", "c", "d"],
+        ),
         # Fig 4b (including spacers for H layers)
-        "Zalt": Circuit([[], ('Gcnot', 'b', 'aux'), [], ('Gcnot', 'd', 'aux'),
-                         ('Gcnot', 'a', 'aux'), ('Gcnot', 'c', 'aux'), [], ('Iz','aux')],
-                        line_labels=['aux', 'a', 'b', 'c', 'd'])
+        "Zalt": Circuit(
+            [
+                [],
+                ("Gcnot", "b", "aux"),
+                [],
+                ("Gcnot", "d", "aux"),
+                ("Gcnot", "a", "aux"),
+                ("Gcnot", "c", "aux"),
+                [],
+                ("Iz", "aux"),
+            ],
+            line_labels=["aux", "a", "b", "c", "d"],
+        ),
     }
 )
 
 ##############
 # SURFACE-25 #
 ##############
-surface25_qubits = [f'D{i}' for i in range(12)] + [f'A{i}' for i in range(12, 15)]
+surface25_qubits = [f"D{i}" for i in range(12)] + [f"A{i}" for i in range(12, 15)]
 
 # For Surface-25, we can perform all stabilizers at once (Fig 1a)
 # Note that for weight-3 stabilizers, we still use the weight-4 template
@@ -46,26 +75,26 @@ surface25_qubits = [f'D{i}' for i in range(12)] + [f'A{i}' for i in range(12, 15
 # i.e. data qubit order should start at the right and go clockwise
 surface25_stabilizers = {
     "X": [
-        ["A13", "D0",  None,  "D1", "D3"],
-        ["A14", "D1",  None,  "D2", "D4"],
-        ["A18", "D5",  "D3",  "D6", "D8"],
-        ["A19", "D6",  "D4",  "D7", "D9"],
+        ["A13", "D0", None, "D1", "D3"],
+        ["A14", "D1", None, "D2", "D4"],
+        ["A18", "D5", "D3", "D6", "D8"],
+        ["A19", "D6", "D4", "D7", "D9"],
         ["A23", "D10", "D8", "D11", None],
-        ["A24", "D11", "D9", "D12", None]
+        ["A24", "D11", "D9", "D12", None],
     ],
     "Z": [
-        ["A15", None, "D0", "D3",  "D5"],
-        ["A16", "D3", "D1", "D4",  "D6"],
-        ["A17", "D4", "D2", None,  "D7"],
+        ["A15", None, "D0", "D3", "D5"],
+        ["A16", "D3", "D1", "D4", "D6"],
+        ["A17", "D4", "D2", None, "D7"],
         ["A20", None, "D5", "D8", "D10"],
         ["A21", "D8", "D6", "D9", "D11"],
-        ["A22", "D9", "D7", None, "D12"]
+        ["A22", "D9", "D7", None, "D12"],
     ],
 }
 surface25_syndrome = SyndromeCircuit(
     factory=surface_factory,
     stabilizers=surface25_stabilizers,
-    qubit_labels=surface25_qubits
+    qubit_labels=surface25_qubits,
 )
 
 # We can also easily use the alternate schedule by swapping out the stabilizer type
@@ -75,28 +104,28 @@ surface25_syndrome_alt = surface25_syndrome.map_stabilizer_keys({"Z": "Zalt"})
 ##############
 # SURFACE-17 #
 ##############
-surface17_qubits = [f'D{i}' for i in range(9)] + [f'A{i}' for i in range(9, 17)]
+surface17_qubits = [f"D{i}" for i in range(9)] + [f"A{i}" for i in range(9, 17)]
 
 # For Surface-17, we can also perform all stabilizers at once (Fig 1b)
 # This time, we have weight-2 checks instead of weight-3
 surface17_stabilizers = {
     "X": [
-        ["A9",  None, None, "D1", "D2"],
+        ["A9", None, None, "D1", "D2"],
         ["A11", "D0", "D1", "D3", "D4"],
         ["A14", "D4", "D5", "D7", "D8"],
-        ["A16", "D6", "D7", None, None]
+        ["A16", "D6", "D7", None, None],
     ],
     "Z": [
         ["A10", None, "D0", None, "D3"],
         ["A12", "D1", "D2", "D4", "D5"],
         ["A13", "D3", "D4", "D6", "D7"],
-        ["A15", "D5", None, "D8", None]
+        ["A15", "D5", None, "D8", None],
     ],
 }
 surface17_syndrome = SyndromeCircuit(
     factory=surface_factory,
     stabilizers=surface17_stabilizers,
-    qubit_labels=surface17_qubits
+    qubit_labels=surface17_qubits,
 )
 
 # Again, can use alternate Z stabilizer schedule
@@ -105,14 +134,14 @@ surface17_syndrome_alt = surface17_syndrome.map_stabilizer_keys({"Z": "Zalt"})
 ##############
 # SURFACE-13 #
 ##############
-surface13_qubits = [f'D{i}' for i in range(9)] + [f'A{i}' for i in range(9, 13)]
+surface13_qubits = [f"D{i}" for i in range(9)] + [f"A{i}" for i in range(9, 13)]
 
 # For Surface-13, we perform the stabilizers in two stages to reuse aux qubits (Fig 1c)
 surface13_stabilizers = [
     {
         # First we do all weight-4 checks
         "X": [
-            ["A9",  "D0", "D1", "D3", "D4"],
+            ["A9", "D0", "D1", "D3", "D4"],
             ["A12", "D4", "D5", "D7", "D8"],
         ],
         "Z": [
@@ -123,21 +152,15 @@ surface13_stabilizers = [
     {
         # And then all weight-2 checks
         # (note that these plaquettes are flipped from Surface-17 weight-2 checks)
-        "X": [
-            ["A10", "D1", "D2", None, None],
-            ["A11", None, None, "D6", "D7"]
-        ],
-        "Z": [
-            ["A9",  "D0", None, "D3", None],
-            ["A12", None, "D5", None, "D8"]
-        ],
-    }
+        "X": [["A10", "D1", "D2", None, None], ["A11", None, None, "D6", "D7"]],
+        "Z": [["A9", "D0", None, "D3", None], ["A12", None, "D5", None, "D8"]],
+    },
 ]
 
 surface13_syndrome = SyndromeCircuit(
     factory=surface_factory,
     stabilizers=surface13_stabilizers,
-    qubit_labels=surface13_qubits
+    qubit_labels=surface13_qubits,
 )
 
 # Again, can use alternate Z stabilizer schedule
--- loqs/core/syndromecircuit.py
+++ loqs/core/syndromecircuit.py
@@ -5,7 +5,8 @@ from typing import Iterable, Mapping, Optional, TypeAlias, Union
 
 from pygsti.circuits import Circuit
 
-class StabilizerPlaquetteFactory():
+
+class StabilizerPlaquetteFactory:
     """Generate stabilizer plaquettes from template circuits.
 
     The idea is to take a set of templates for stabilizer plaquettes
@@ -26,12 +27,12 @@ class StabilizerPlaquetteFactory():
 
     >>> templates = {
     ...     # Fig 2a of [1]_
-    ...     "X": Circuit([('Gh', 'aux'), ('Gcnot', 'aux', 'b'), ('Gcnot', 'aux', 'a'), 
+    ...     "X": Circuit([('Gh', 'aux'), ('Gcnot', 'aux', 'b'), ('Gcnot', 'aux', 'a'),
     ...                 ('Gcnot', 'aux', 'd'), ('Gcnot', 'aux', 'c'), ('Gh', 'aux'),
     ...                 ('Iz', 'aux')],
     ...                 line_labels=['a', 'b', 'c', 'd', 'aux']),
     ...     # Fig 2b of [1]_ (including idle layers to match X check H layers)
-    ...     "Z": Circuit([[], ('Gcnot', 'b', 'aux'), ('Gcnot', 'a', 'aux'), 
+    ...     "Z": Circuit([[], ('Gcnot', 'b', 'aux'), ('Gcnot', 'a', 'aux'),
     ...                 ('Gcnot', 'd', 'aux'), ('Gcnot', 'c', 'aux'), [], ('Iz','aux')],
     ...                 line_labels=['a', 'b', 'c', 'd', 'aux']),
     ... }
@@ -50,7 +51,7 @@ class StabilizerPlaquetteFactory():
     Circuit(Gh:A4Gcnot:A4:D1[]Gcnot:A4:D3[]Gh:A4Iz:A4@(D1,D3,A4))
 
     Creation of a weight-4 stabilizer *preparation* circuit.
-    
+
     >>> c = factory.get_circuit("X", ["D0", "D1", "D2", "D3", "A4"], omit_gates="Iz")
     >>> print(repr(c))
     Circuit(Gh:A4Gcnot:A4:D1Gcnot:A4:D0Gcnot:A4:D3Gcnot:A4:D2Gh:A4[]@(D0,D1,D2,D3,A4))
@@ -58,21 +59,26 @@ class StabilizerPlaquetteFactory():
     .. [1] Y. Tomita and K.M. Svore, "Low-distance surface codes under realistic
         quantum simulations," Phys. Rev. A, vol. 90, pp. 062320, 2014.
     """
-    def __init__(self, stabilizer_templates: Mapping[str, Circuit])-> None:
+
+    def __init__(self, stabilizer_templates: Mapping[str, Circuit]) -> None:
         """Initialize a StabilizerPlaquetteFactory from stabilizer templates.
-        
+
         Parameters
         ----------
         stabilizer_templates: dict
             Keys will be string labels for each stabilizer type,
             and values will be pyGSTi Circuits.
-        """ 
+        """
         self.stabilizer_templates = stabilizer_templates
-    
-    def get_circuit(self, template_key: str, qubit_labels: Iterable[Optional[str]],
-                    omit_gates: Optional[Union[Iterable[str], str]] = None) -> Circuit:
+
+    def get_circuit(
+        self,
+        template_key: str,
+        qubit_labels: Iterable[Optional[str]],
+        omit_gates: Optional[Union[Iterable[str], str]] = None,
+    ) -> Circuit:
         """Create a stabilizer check circuit from one of the templates.
-    
+
         If removing a line or operation results in an empty layer,
         that layer is retained to ensure no scheduling changes
         in the remainder of the circuit.
@@ -81,28 +87,29 @@ class StabilizerPlaquetteFactory():
         ----------
         template_key: str
             Stabilizer template key for self.stabilizer_templates
-        
+
         qubit_labels: list of str or None
             Qubit labels that match exactly with the template line_labels.
             If an entry is None, then that template line_label (and all
             gates that touch that line) are removed.
             For example, lower-weight checks with a commensurate
             schedule can be implemented this way.
-        
+
         omit_gates: str or list of str, optional
             Gatenames that should be removed from the final circuit,
             For example, syndrome preparation (rather than extraction)
             can be implemented by omiting the midcircuit measurements.
-        
+
         Returns
         -------
         circuit: pygsti.circuits.Circuit
             The qubit-replaced stabilizer check circuit
         """
         circuit = self.stabilizer_templates[template_key].copy(editable=True)
-        assert len(qubit_labels) == len(circuit.line_labels), \
-            "Number of qubit labels should match template lines. " + \
-            "If dropping lines is desired, use None in correct place in qubit_labels."
+        assert len(qubit_labels) == len(circuit.line_labels), (
+            "Number of qubit labels should match template lines. "
+            + "If dropping lines is desired, use None in correct place in qubit_labels."
+        )
 
         # Either map the qubit or remove it if providing None
         # The latter feature is useful to get lower weight checks with the same schedule
@@ -112,68 +119,84 @@ class StabilizerPlaquetteFactory():
                 circuit.delete_lines(template_qubit, delete_straddlers=True)
             else:
                 mapping[template_qubit] = qubit
-        
+
         circuit.map_state_space_labels_inplace(mapping)
-        
+
         # Optionally omit some gates, e.g. a measurement if interested
         # in the syndrome preparation rather than extraction circuit
         if omit_gates is not None:
             if isinstance(omit_gates, str):
                 omit_gates = [omit_gates]
-            
+
             for gate in omit_gates:
                 circuit.replace_gatename_with_idle_inplace(gate)
-        
+
         circuit.done_editing()
         return circuit
 
-    
+
 ListStrMap: TypeAlias = Mapping[str, Iterable[str]]
-class SyndromeCircuit():
-    def __init__(self, factory: StabilizerPlaquetteFactory,
-                 stabilizers: Union[ListStrMap, Iterable[ListStrMap]],
-                 qubit_labels: Iterable[str]) -> None:
+
+
+class SyndromeCircuit:
+    def __init__(
+        self,
+        factory: StabilizerPlaquetteFactory,
+        stabilizers: Union[ListStrMap, Iterable[ListStrMap]],
+        qubit_labels: Iterable[str],
+    ) -> None:
         self.factory = factory
         if isinstance(stabilizers, (list, tuple)):
             self.stabilizer_stages = stabilizers
         else:
             # We only got one stage, make it a list for consistency
             self.stabilizer_stages = [stabilizers]
-        
+
         for stage in self.stabilizer_stages:
-            assert all([c in factory.stabilizer_templates.keys() for c in stage.keys()]), \
-                "Not all provided checks can be created by the provided factory"
-        
+            assert all(
+                [c in factory.stabilizer_templates.keys() for c in stage.keys()]
+            ), "Not all provided checks can be created by the provided factory"
+
         self.qubit_labels = qubit_labels
-    
-    def get_circuit(self, omit_gates: Optional[Union[Iterable[str], str]] = None,
-                    compress_within_stages: bool = False,
-                    compress_between_stages: bool = False,
-                    delete_idle_layers: bool = True) -> Circuit:
+
+    def get_circuit(
+        self,
+        omit_gates: Optional[Union[Iterable[str], str]] = None,
+        compress_within_stages: bool = False,
+        compress_between_stages: bool = False,
+        delete_idle_layers: bool = True,
+    ) -> Circuit:
         if qubit_labels is None:
-            qubit_labels = 'auto'
-        
+            qubit_labels = "auto"
+
         circuit = Circuit([], line_labels=self.qubit_labels, editable=True)
         for stage in self.stabilizer_stages:
             stage_layers = []
             for i, (key, qubit_list) in enumerate(stage.items()):
                 for j, qubits in enumerate(qubit_list):
-                    check_circuit = self.factory.get_circuit(key, qubits, omit_gates=omit_gates)
+                    check_circuit = self.factory.get_circuit(
+                        key, qubits, omit_gates=omit_gates
+                    )
                     if i == 0 and j == 0:
                         # Just take layers directly
-                        stage_layers.extend([list(check_circuit.layer(j)) for j in range(check_circuit.num_layers)])
+                        stage_layers.extend(
+                            [
+                                list(check_circuit.layer(j))
+                                for j in range(check_circuit.num_layers)
+                            ]
+                        )
                     else:
                         # Concatenate layers
                         for k in range(check_circuit.num_layers):
                             if k > len(stage_layers) - 1:
                                 stage_layers.append([])
-                            
+
                             stage_layers[k] += list(check_circuit.layer(k))
 
             # TODO: I expect this to fail on a collision
             # Figure out what that error is and put a nice error message
             stage_circuit = Circuit(stage_layers, line_labels=qubit_labels)
-            
+
             if compress_within_stages:
                 stage_circuit = stage_circuit.parallelize()
 
@@ -189,10 +212,10 @@ class SyndromeCircuit():
 
         return circuit
 
-    def map_stabilizer_keys(self, key_mapping: Mapping[str, str]) -> 'SyndromeCircuit':
+    def map_stabilizer_keys(self, key_mapping: Mapping[str, str]) -> "SyndromeCircuit":
         new_stabilizer_stages = []
         for stage in self.stabilizer_stages:
-            new_stage = {key_mapping.get(k, k): v for k,v in stage.items()}
+            new_stage = {key_mapping.get(k, k): v for k, v in stage.items()}
             new_stabilizer_stages.append(new_stage)
-        
+
         return SyndromeCircuit(self.factory, new_stabilizer_stages, self.qubit_labels)
