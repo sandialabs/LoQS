cbaa6654c054298d6edd1cc98c813bc36ccdc18e
Riley Murray <rjmurr@sandia.gov>
2026-03-10 16:28:29 -0700

    tests for jsonencoding.py

--- loqs/internal/jsonencoding.py
+++ loqs/internal/jsonencoding.py
@@ -2,6 +2,12 @@
 """
 
 import json
+from typing import Any, Callable
+from warnings import warn
+
+
+class JSONEncodingFailure(UserWarning):
+    pass
 
 
 class JSONEncoderWithErrors(json.JSONEncoder):
@@ -11,17 +17,20 @@ class JSONEncoderWithErrors(json.JSONEncoder):
     JSON encoding issues.
     """
 
+    def __init__(self, *, skipkeys: bool = False, ensure_ascii: bool = True, check_circular: bool = True, allow_nan: bool = True, sort_keys: bool = False, indent: int | str | None = None, separators: tuple[str, str] | None = None, default: Callable[..., Any] | None = None) -> None:
+        super().__init__(skipkeys=skipkeys, ensure_ascii=ensure_ascii, check_circular=check_circular, allow_nan=allow_nan, sort_keys=sort_keys, indent=indent, separators=separators, default=default)
+        self.key : Any = None
+
     def default(self, o):
         try:
             return json.JSONEncoder.default(self, o)
         except TypeError as e:
-            print(f"Error serializing object: {o}, Key: {self.key}")
+            e.args = (f"Error serializing object: {o}, Key: {self.key}",)
             raise e
 
 
 def dump_or_dumps_with_error_handling(data, f=None):
     encoder = JSONEncoderWithErrors()
-    encoder.key = None  # Initialize key for tracking
 
     def encode_with_key_tracking(obj):
         if isinstance(obj, dict):
@@ -38,4 +47,6 @@ def dump_or_dumps_with_error_handling(data, f=None):
         else:
             return json.dumps(data, cls=JSONEncoderWithErrors)
     except TypeError as e:
-        print(f"Error during JSON serialization: {e}")
+        warn(str(e), JSONEncodingFailure)
+
+    return
